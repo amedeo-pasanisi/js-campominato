7cdff4551ff79f552ff76bd8e87b599a38246045
amedeo <amedeopasanisi97@gmail.com>
2021-07-26 12:09:08 +0200

    il computer genera 16 volte un numero casuale

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-// Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 // I numeri non possono essere duplicati.
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+for (var i = 0; i < 16; i++) {
+    getRndInteger(1, 100);
+}
