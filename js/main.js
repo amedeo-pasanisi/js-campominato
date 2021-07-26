f4246c6f2cdaad83d4774b40aca25dd2c8230512
amedeo <amedeopasanisi97@gmail.com>
2021-07-26 17:46:18 +0200

    correzione "fine esercizio per console"

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function getRndInteger(min, max) {
 // I numeri non possono essere duplicati.
 var bombe = [];
 while (bombe.length < 16) {
-    rndNum = getRndInteger(1, 20);
+    var rndNum = getRndInteger(1, 100);
     if (bombe.includes(rndNum) == false) {
         bombe.push(rndNum);
     }
@@ -20,28 +20,31 @@ do // ripeti finchè l'array campo non è pieno o finchè il numero inserito non
 {
     var numUtente = parseInt(prompt("inserisci un numero compreso tra 1 e 100"));
     // L’utente non può inserire più volte lo stesso numero altrimenti si continua chiedendo all’utente un altro numero.
-    if (campo.includes(numUtente) == false) {
+    if ((campo.includes(numUtente) == false) && (isNaN(numUtente) == false)) {
         campo.push(numUtente);
         console.log ("numero inserito: " + numUtente);
         punteggio++;
-    } else if (campo.includes(numUtente) == true) {
-        while (campo.includes(numUtente) == true) {
-            numUtente = parseInt(prompt("hai già inserito questo numero, inserisci un'altro numero compreso tra 1 e 100"));
+    } else {
+        while ((campo.includes(numUtente) == true) || (isNaN(numUtente) == true)) {
+            numUtente = parseInt(prompt("hai già inserito questo numero oppure non hai inserito nessun numero. inserisci un numero compreso tra 1 e 100"));
         }
         campo.push(numUtente);
         console.log ("numero inserito: " + numUtente);
         punteggio++;
     }
 }
-while ((campo.length < 20 - bombe.length) && (bombe.includes(numUtente) == false));
+while ((campo.length < 100 - bombe.length) && (bombe.includes(numUtente) == false));
 
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-if (campo.length == 20 - bombe.length) {
+if (campo.length == 20 - bombe.length)
+{
     console.log("partita conclusa, hai totalizzato " + punteggio + " punti");
     console.log("hai raggiunto il punteggio massimo");
-} else if (bombe.includes(numUtente) == true) {
+}
+else if (bombe.includes(numUtente) == true)
+{
     console.log ("bombe: " + bombe.sort(function(a, b){return a - b})); //per ordinare numericamente le bombe
-    console.log(numUtente + " è una bomba")
+    console.log(numUtente + " è una bomba");
     console.log("partita conclusa, hai totalizzato " + punteggio + " punti");
 }
