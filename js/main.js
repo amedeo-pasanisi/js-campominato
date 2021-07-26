ac35be55a2ef65bdbfe87c155a3b0fd2bf810269
amedeo <amedeopasanisi97@gmail.com>
2021-07-26 17:59:24 +0200

    correzione 2 "fine esercizio per console"

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ while (bombe.length < 16) {
     }
 }
 
-// il computer deve chiedere all’utente (100 - il numero delle bombe) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// il computer deve chiedere all’utente (100 - numero delle bombe) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 var campo = [];
 var punteggio = 0;
 
@@ -20,13 +20,13 @@ do // ripeti finchè l'array campo non è pieno o finchè il numero inserito non
 {
     var numUtente = parseInt(prompt("inserisci un numero compreso tra 1 e 100"));
     // L’utente non può inserire più volte lo stesso numero altrimenti si continua chiedendo all’utente un altro numero.
-    if ((campo.includes(numUtente) == false) && (isNaN(numUtente) == false)) {
+    if ((campo.includes(numUtente) == false) && (isNaN(numUtente) == false) && (numUtente <= 100)) {
         campo.push(numUtente);
         console.log ("numero inserito: " + numUtente);
         punteggio++;
     } else {
-        while ((campo.includes(numUtente) == true) || (isNaN(numUtente) == true)) {
-            numUtente = parseInt(prompt("hai già inserito questo numero oppure non hai inserito nessun numero. inserisci un numero compreso tra 1 e 100"));
+        while ((campo.includes(numUtente) == true) || (isNaN(numUtente) == true) || (numUtente > 100)) {
+            numUtente = parseInt(prompt("hai già inserito questo numero oppure non hai inserito nessun numero valido. inserisci un numero compreso tra 1 e 100"));
         }
         campo.push(numUtente);
         console.log ("numero inserito: " + numUtente);
@@ -37,13 +37,10 @@ while ((campo.length < 100 - bombe.length) && (bombe.includes(numUtente) == fals
 
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-if (campo.length == 20 - bombe.length)
-{
+if (campo.length == 20 - bombe.length) {
     console.log("partita conclusa, hai totalizzato " + punteggio + " punti");
     console.log("hai raggiunto il punteggio massimo");
-}
-else if (bombe.includes(numUtente) == true)
-{
+} else if (bombe.includes(numUtente) == true) {
     console.log ("bombe: " + bombe.sort(function(a, b){return a - b})); //per ordinare numericamente le bombe
     console.log(numUtente + " è una bomba");
     console.log("partita conclusa, hai totalizzato " + punteggio + " punti");
