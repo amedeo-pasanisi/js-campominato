6c1a05dd8f3513c3cb397f002eaac2058b3ae8be
amedeo <amedeopasanisi97@gmail.com>
2021-07-26 12:54:52 +0200

    16 numeri random senza duplicati

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// I numeri non possono essere duplicati.
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
@@ -10,6 +9,11 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+// I numeri non possono essere duplicati.
 for (var i = 0; i < 16; i++) {
-    getRndInteger(1, 100);
+    do {
+        var rndNum = getRndInteger(1, 100);
+    } while (rndNum - rndNum != 0);
+    console.log(rndNum);
 }
+
